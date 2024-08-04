eb6fead4bdb1ef69a772ae1d3f2cc5c7afba2c6e
ahmetarmutcu <ahmetarmutcu2017@gmail.com>
2024-08-04 13:24:13 +0300

    Rabbit mq ve azure service bus için test methodları eklendi.

---
+++ EventBus.UnitTest/EventBusTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Web.Services.Description;
+
+namespace EventBus.UnitTest
+{
+	[TestClass]
+	public class EventBusTests
+	{
+		private ServiceCollection
+
+
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
---
+++ EventBus.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EventBus.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EventBus.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("348a17ad-d03f-405e-9335-9d31ccd1d150")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
